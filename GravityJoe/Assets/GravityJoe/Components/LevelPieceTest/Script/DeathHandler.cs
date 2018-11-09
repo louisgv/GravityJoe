13b97b3c391c2cc9a506c54cb8d598d89ad0b692
ejp4604 <ejp4604@g.rit.edu>
2018-11-09 14:33:20 -0500

    Comments

--- GravityJoe/Assets/GravityJoe/Components/LevelPieceTest/Script/DeathHandler.cs
+++ GravityJoe/Assets/GravityJoe/Components/LevelPieceTest/Script/DeathHandler.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class DeathHandler : MonoBehaviour {
 
+    // player info to find collision for this platform object
     public GameObject player;
-    public bool collision;
+    bool collision;
     public LayerMask playerMask;
    
     // Update is called once per frame
     void Update () {
+        // checks if overlap box is overlapping with player mask
         collision = Physics2D.OverlapBox(new Vector2(transform.position.x, transform.position.y), new Vector2(transform.localScale.x - .1f, transform.localScale.y - .1f), 0f, playerMask);
 
+        // if colliding, destroy objects
         if (collision)
             Destroy(player);
     }
--- GravityJoe/Assets/GravityJoe/Core/GameManager.cs
+++ GravityJoe/Assets/GravityJoe/Core/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
     
+    // base objects for instantiation
     [SerializeField]
     GameObject playerBase;
     [SerializeField]
@@ -15,6 +16,7 @@ public class GameManager : MonoBehaviour {
     [SerializeField]
     GameObject destructibleWallBase;
 
+    // current gameObjects in scene
     public GameObject currentPlayer;
     public GameObject currentPlatform;
     public GameObject currentDeathObject;
@@ -27,16 +29,20 @@ public class GameManager : MonoBehaviour {
         // check if player already exists, if not, spawn one
         if (GravityJoe.Utility.GetPlayer() == null)
             currentPlayer = Instantiate(playerBase, Vector3.zero, Quaternion.identity);
+
+        // creates one platform and one death object for testing
         currentPlatform = Instantiate(platformBase, new Vector3(0, -5f, 0), Quaternion.identity);
         currentDeathObject = Instantiate(deathObjectBase, new Vector3(3.5f, -4f, 0), Quaternion.identity);
 
+        // sets death object's player GameObject to the current player object (for testing)
         currentDeathObject.GetComponent<DeathHandler>().player = currentPlayer;
-        //Camera.SetupCurrent(currentPlayer.GetComponentInChildren<Camera>());
         
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        // resets player and player object in death object if the player dies (for testing)
         if (currentPlayer == null)
         {
             currentPlayer = Instantiate(playerBase, Vector3.zero, Quaternion.identity);
@@ -45,11 +51,12 @@ public class GameManager : MonoBehaviour {
             
     }
 
+    // used to reset destructible objects
     void ResetDestructibleObjects()
     {
 
     }
-
+    // used to reset level orientation
     void ResetLevelOrientation()
     {
 
